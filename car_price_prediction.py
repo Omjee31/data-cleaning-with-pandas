490ff68f4ae4d98f003c06288b0f59b9d3810f0b
RAJPUT_45 <151171172+Omjee31@users.noreply.github.com>
2026-02-15 23:07:26 +0530

    Enhance car price prediction model with training and evaluation
    Updated the car price prediction script to include data encoding, model training, and evaluation metrics.

---
+++ car_price_prediction.py
@@ -0,0 +1,50 @@
+
+
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+# Pour outliers avancés :
+from sklearn.ensemble import IsolationForest
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+
+#'''path = r"C:\Users\omjee\.cache\kagglehub"
+
+#for dirname, _, filenames in os.walk(path):
+ #   for filename in filenames:
+       # print(os.path.join(dirname, filename))'''
+
+
+
+df = pd.read_csv("car_price_prediction_.csv")
+df_encoded = pd.get_dummies(df, columns=['model', 'transmission', 'fuelType', 'engineSize','mpg','mileage','tax'])
+
+# Features & target
+X = df_encoded.drop('price', axis=1)
+y = df_encoded['price']
+
+# Spliting train/test (80%/20%)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# traîning the model
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+
+# prediction over a handset 
+y_pred = model.predict(X_test)
+# Evaluating of the performances
+print('MAE :', mean_absolute_error(y_test, y_pred))
+print('MSE :', mean_squared_error(y_test, y_pred))
+print('R² :', r2_score(y_test, y_pred))
+
+pred_df = X_test.copy()
+pred_df['price_true'] = y_test
+pred_df['price_pred'] = y_pred
+print(pred_df[['price_true', 'price_pred']].head())
